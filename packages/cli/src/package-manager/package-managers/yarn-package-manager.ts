ff085c41384028125720e4afa1ce899c26b55322
ziho <ziho@toss.im>
2026-01-20 21:03:23 +0900

    fix: resolve package paths relative to cwd

--- packages/cli/src/package-manager/package-managers/yarn-package-manager.ts
+++ packages/cli/src/package-manager/package-managers/yarn-package-manager.ts
@@ -1,6 +1,8 @@
 import { execSync } from "child_process";
 import { z } from "zod";
 import { PackageManager, Package } from "../types/package-manager.type.js";
+import path from "path";
+import { findUp } from "../utils/find-up.js";
 
 const workspaceIdentitySchema = z.object({
   name: z.string().nullable(),
@@ -8,20 +10,38 @@ const workspaceIdentitySchema = z.object({
 });
 
 export class YarnPackageManager implements PackageManager {
-  constructor(private cwd: string) {}
-  
+  constructor(private cwd: string) { }
+
   getPackages(): Package[] {
     try {
       const output = execSync("yarn workspaces list --json", { encoding: "utf8", cwd: this.cwd });
+      const repoRootPath = this.getRepoRootPath();
 
       return output
         .split("\n")
         .filter(Boolean)
         .map((line) => workspaceIdentitySchema.parse(JSON.parse(line)))
         .filter((ws) => ws.name !== null)
-        .map((ws) => ({ name: ws.name as string, location: ws.location }));
+        .map((ws) => {
+          const locationAbsolutePath = path.join(repoRootPath, ws.location);
+
+          return {
+            name: ws.name as string,
+            location: path.relative(this.cwd, locationAbsolutePath)
+          };
+        });
     } catch {
       return [];
     }
+  }
+
+  private getRepoRootPath(): string {
+    const root = findUp("yarn.lock", this.cwd);
+
+    if (root == null) {
+      throw new Error(`'yarn.lock' not found from ${this.cwd}`);
+    }
+
+    return path.dirname(root);
   }
 }
