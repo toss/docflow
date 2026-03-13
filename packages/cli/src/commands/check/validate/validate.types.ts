28c09104d0b11291c5e8fed01282c2d096de6ae6
ziho <ziho@toss.im>
2026-03-13 21:02:37 +0900

    fix: format

--- packages/cli/src/commands/check/validate/validate.types.ts
+++ packages/cli/src/commands/check/validate/validate.types.ts
@@ -1,9 +1,4 @@
-type ValidationErrorType =
-  | "missing_public"
-  | "missing_param"
-  | "unused_param"
-  | "missing_returns"
-  | "invalid_returns";
+type ValidationErrorType = "missing_public" | "missing_param" | "unused_param" | "missing_returns" | "invalid_returns";
 
 export interface ValidationError {
   type: ValidationErrorType;
--- packages/cli/src/tests/utils/create-ts-source-file.ts
+++ packages/cli/src/tests/utils/create-ts-source-file.ts
@@ -1,6 +1,6 @@
-import { Project, SourceFile } from 'ts-morph';
+import { Project, SourceFile } from "ts-morph";
 
 export function createTSSourceFile(code: string): SourceFile {
-    const project = new Project({ useInMemoryFileSystem: true });
-    return project.createSourceFile("test.ts", code);
+  const project = new Project({ useInMemoryFileSystem: true });
+  return project.createSourceFile("test.ts", code);
 }
