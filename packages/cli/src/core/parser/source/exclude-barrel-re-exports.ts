46e55e518292afcce3389056138c40894932679f
ziho <ziho@toss.im>
2026-03-26 01:54:37 +0900

    fix: ensure excludeBarrelReExports preserves exports with same name but different kind

--- packages/cli/src/core/parser/source/exclude-barrel-re-exports.ts
+++ packages/cli/src/core/parser/source/exclude-barrel-re-exports.ts
@@ -2,7 +2,7 @@ import { groupBy } from "es-toolkit";
 import { ExportDeclaration } from "../../types/parser.types.js";
 
 export function excludeBarrelReExports(exportDeclarations: ExportDeclaration[]): ExportDeclaration[] {
-  return Object.values(groupBy(exportDeclarations, exp => exp.symbolName))
+  return Object.values(groupBy(exportDeclarations, exp => `${exp.symbolName}:${exp.kind}`))
     .flatMap(declarations => {
       if (declarations.length === 1) {
         return declarations;
